0b675a8cd62b0ab4d3d44ad62ac3f8cb1b4e45bd
epopov <mr.demix@yandex.ru>
2022-08-14 16:55:28 +0300

    Added simple spring security configuration.

--- src/main/java/ru/epopov/legitcheckportal/security/ApplicationSecurityConfiguration.java
+++ src/main/java/ru/epopov/legitcheckportal/security/ApplicationSecurityConfiguration.java
@@ -1,21 +1,27 @@
 package ru.epopov.legitcheckportal.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-//@EnableWebSecurity
-//@Configuration
-//public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
-//
-//  @Override
-//  protected void configure(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeRequests()
-//        .anyRequest()
-//        .authenticated()
-//        .and()
-//        .httpBasic();
-//  }
-//}
+@EnableWebSecurity
+@Configuration
+public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .anyRequest()
+        .authenticated()
+        .and()
+        .httpBasic();
+  }
+}
