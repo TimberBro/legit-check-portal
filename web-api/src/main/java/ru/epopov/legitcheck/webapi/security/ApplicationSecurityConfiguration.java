ea5525df2c0646c64fb5fd35a07034b4bd4d001c
desktop-epopov\epopov <mr.demix@yandex.ru>
2023-02-06 20:59:38 +0300

    Edit old configuration

--- web-api/src/main/java/ru/epopov/legitcheck/webapi/security/ApplicationSecurityConfiguration.java
+++ web-api/src/main/java/ru/epopov/legitcheck/webapi/security/ApplicationSecurityConfiguration.java
@@ -3,14 +3,7 @@ package ru.epopov.legitcheck.webapi.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -19,30 +12,18 @@ public class ApplicationSecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeRequests((requests) -> requests
-            .antMatchers("/items", "/items/**")
-            .hasAuthority("Customer"))
+    http.authorizeRequests()
+        .antMatchers("/h2-console/**")
+        .permitAll()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/items", "/items/**")
+        .hasAuthority("Customer")
+        .anyRequest()
+        .authenticated()
+        .and()
         .httpBasic();
 
     return http.build();
   }
-
-  /*
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        .authorizeRequests()
-        .antMatchers("/items", "/items/**")
-  }
-
-   */
-
-  /*
-  @Override
-  public void configure(WebSecurity web) throws Exception {
-    web
-        .ignoring()
-        .antMatchers("/h2-console/**");
-  }
-   */
 }
